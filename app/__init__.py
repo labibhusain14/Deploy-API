302fc1ce6b237319ce4a8e99163b8b84fc0726fd
Mohammad Labib <119772365+labibhusain14@users.noreply.github.com>
2023-12-18 16:07:02 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,18 @@ from .models import User
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/db_minatku"
+    # Menggunakan public IP address dan connection name yang disediakan
+    db_config = {
+        'user': 'root',
+        'password': 'minatkuapp',  # Masukkan password database Anda di sini
+        'db': 'db_minatku',
+        'host': '34.128.74.245',
+        'unix_socket': '/cloudsql/deploy-api-408210:asia-southeast2:minatkudb'
+    }
+
+    # Mengonfigurasi SQLAlchemy untuk menggunakan MySQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['db']}?unix_socket={db_config['unix_socket']}"
+
     app.config["JWT_SECRET_KEY"] = "thisisasecret"
   
     api.init_app(app)
